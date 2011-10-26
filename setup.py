586571aea38a77a1fcdf895f6323072b1d255b6c
Adam <gekitsuu@gmail.com>
2011-10-26 16:05:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ version = '0.1'
 
 setup(name='instapy',
       version=version,
-      description="Python Library for instapaper.com API",
+      description="Python Library for the instapaper.com Simple API",
       long_description="""\
+This is a python library to give access to instapaper's simple API but in the future I would like to include the more avanced API too.
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='instapaper',
       author='Adam Glenn',
       author_email='gekitsuu@gmail.com',
-      url='http://gekitsuu.org',
+      url='https://github.com/gekitsuu/instapy',
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
